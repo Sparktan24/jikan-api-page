62b8c579c2119f185f3b0ec21b3273a0e9842f02
Jesus Molina <netod90@gmail.com>
2023-09-21 18:50:15 -0700

    Add useAnimes hook

--- src/components/Animes.jsx
+++ src/components/Animes.jsx
@@ -1,13 +1,31 @@
-import dataMock from '../mocks/data-response.json';
+import { useEffect, useState } from 'react';
+import { getAllAnimes } from '../services/animes';
+
+function useAnimes() {
+  const [animes, setAnimes] = useState([]);
+
+  const getAnimes = async () => {
+    const data = await getAllAnimes();
+    setAnimes(data);
+  };
+
+  useEffect(() => {
+    getAnimes();
+  }, []);
+
+  console.log(animes);
+  return { data: animes };
+}
 
 const ListOfAnimes = () => {
-  const data = dataMock.data;
+  const { data } = useAnimes();
+
   return (
     <ul className="animes">
-      {data.map((anime) => (
-        <li className="anime" key={anime.mal_id}>
+      {data?.map((anime) => (
+        <li className="anime" key={anime.id}>
           <h3>{anime.title}</h3>
-          <img src={anime.images.jpg.image_url} />
+          <img src={anime.image} />
           <p>{anime.synopsis}</p>
         </li>
       ))}
--- src/services/animes.js
+++ src/services/animes.js
@@ -1,16 +1,13 @@
-//  import dataMock from '../mocks/data-response.json';
-
 export const getAllAnimes = async () => {
-  /* const animes = dataMock.data;
-  return animes?.map((anime) => ({
+  const response = await fetch('https://api.jikan.moe/v4/anime');
+  const data = await response.json();
+  const animeList = data.data;
+  console.log('teched data', animeList);
+
+  return animeList?.map((anime) => ({
     id: anime.mal_id,
     title: anime.title,
     image: anime.images.jpg.image_url,
     synopsis: anime.synopsis,
-  })); */
-
-  const response = await fetch('https://api.jikan.moe/v4/anime');
-  const data = await response.json();
-  const animeList = data.data;
-  console.log(animeList);
+  }));
 };
