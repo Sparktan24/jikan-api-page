22bfc78952e8f170623e49a6f7920e870e6d99e1
Jesus Molina <netod90@gmail.com>
2023-09-29 02:35:08 -0700

    Add basic search desing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ function App() {
     <div className="page">
       <header>
         <h1>Anime List</h1>
+        <form>
+          <input
+            type="text"
+            name="query"
+            placeholder="Naruto, One piece, Trigun ..."
+          />
+          <button>Buscar</button>
+        </form>
       </header>
       <main>
         <Animes />
