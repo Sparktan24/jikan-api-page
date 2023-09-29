1f20227965c17b2956878fd7d71a38e8bebddd1b
Jesus Molina <netod90@gmail.com>
2023-09-29 02:59:46 -0700

    Add handleChange in input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Animes from './components/Animes';
 
 function App() {
+  const [search, updateSearch] = useState('');
+
+  const handleChange = (e) => {
+    const newSearch = e.target.value;
+    updateSearch(newSearch);
+  };
+
   return (
     <div className="page">
       <header>
@@ -9,9 +17,12 @@ function App() {
         <form>
           <input
             type="text"
+            value={search}
+            onChange={handleChange}
             name="query"
             placeholder="Naruto, One piece, Trigun ..."
           />
+          <p>{search}</p>
           <button>Buscar</button>
         </form>
       </header>
